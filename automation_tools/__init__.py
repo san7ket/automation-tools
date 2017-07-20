14ac3ca784012d2e0e542d15d2e97c19fc968d11
san7ket <san7ketjagtap@gmail.com>
2017-07-20 10:41:07 +0530

    WIP

--- automation_tools/__init__.py
+++ automation_tools/__init__.py
@@ -1053,7 +1053,7 @@ def vm_create():
     run(command)
 
     # Give some time to machine boot
-    time.sleep(300)
+    time.sleep(400)
 
     # Fetch Ip information via ping only when not using VLAN Bridges as we
     # know the VM's Ip address beforehand.
